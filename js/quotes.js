8c7efe424f7f5ca50a7ffdde7cbb45b886f2de76
y-labz <luke000skywalker@gmail.com>
2025-06-16 15:37:21 +0200

    another quick commit...

--- js/quotes.js
+++ js/quotes.js
@@ -1,4 +1,5 @@
 const quotes = [
+"If imaginary numbers aren't real, then maybe negative numbers are imaginary too. – Y",
 "Don’t drink and derive.",
 "Marge, I agree with you in theory. IN THEORY communism works. In theory. — Homer Simpson",
 "If gravity sucks — go flying",
@@ -14,7 +15,7 @@ const quotes = [
 "I have not failed. I've just found 10,000 ways that won't work. – Thomas Edison",
 "Young man, in mathematics you don't understand things. You just get used to them. – John von Neumann",
 "If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is. – John von Neumann",
-"All models are wrong, but some are useful – George E. P. Box",
+"All models are wrong, but some are useful. – George E. P. Box",
 "The scientist does not study nature because it is useful to do so. He studies it because he takes pleasure in it, and he takes pleasure in it because it is beautiful. – Henri Poincaré",
 // "The mathematician plays a game in which he himself invents the rules while the physicist plays a game in which the rules are provided by nature, but as time goes on it becomes increasingly evident that the rules which the mathematician finds interesting are the same as those which nature has chosen. – Paul A.M. Dirac", //good one but too long
 ];
